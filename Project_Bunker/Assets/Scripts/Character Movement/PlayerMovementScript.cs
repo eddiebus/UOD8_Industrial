17188ff375367b393f6b2e82b62836a19c9642f3
Snoopyloopy24 <99731782+Snoopyloopy24@users.noreply.github.com>
2022-05-24 11:33:04 +0100

    mouselook and playermovment can freeze

--- Project_Bunker/Assets/Scripts/Character Movement/PlayerMovementScript.cs
+++ Project_Bunker/Assets/Scripts/Character Movement/PlayerMovementScript.cs
@@ -10,39 +10,53 @@ public class PlayerMovementScript : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-
     public Transform groundcheck;
     public float groundDistance = 0.1f;
-    public LayerMask groundMask; 
-
+    public LayerMask groundMask;
+    public bool Active = true;
     Vector3 velocity;
     bool isGrounded;
+
+ 
+    public void SetActive(bool newBool)
+    {
+        Active = newBool;
+    }
+
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
 
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Debug.Log($"X Move Input {x}");
-        Debug.Log($"Z Move Input {z}");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Active == true)
         {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+
+            if (Debug.isDebugBuild)
+            {
+                Debug.Log($"X Move Input {x}");
+                Debug.Log($"Z Move Input {z}");
+            }
         }
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity);
+
     }
 }
+
+
+
+
+
+
